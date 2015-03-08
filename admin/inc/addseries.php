42a570ea2029bacfc61a977ebad7a74ef8e37280
xKairu <iamnotkyle@live.com>
2015-03-08 18:26:04 +1300

    Add gametype to addseries admin panel

--- admin/inc/addseries.php
+++ admin/inc/addseries.php
@@ -13,18 +13,19 @@ $event = $raw[1];
 $teamA = $_POST["teamA"];
 $teamB = $_POST["teamB"];
 $comment = $_POST["comment"];
+
 $maps = array();
-array_push($maps, $_POST["map1"]);
-array_push($maps, $_POST["map2"]);
-array_push($maps, $_POST["map3"]);
-array_push($maps, $_POST["map4"]);
-array_push($maps, $_POST["map5"]);
-array_push($maps, $_POST["map6"]);
-array_push($maps, $_POST["map7"]);
-array_push($maps, $_POST["map8"]);
-array_push($maps, $_POST["map9"]);
-array_push($maps, $_POST["map10"]);
-array_push($maps, $_POST["map11"]);
+array_push($maps, array("url" => $_POST["map1"], "type" => $_POST["map1type"]));
+array_push($maps, array("url" => $_POST["map2"], "type" => $_POST["map2type"]));
+array_push($maps, array("url" => $_POST["map3"], "type" => $_POST["map3type"]));
+array_push($maps, array("url" => $_POST["map4"], "type" => $_POST["map4type"]));
+array_push($maps, array("url" => $_POST["map5"], "type" => $_POST["map5type"]));
+array_push($maps, array("url" => $_POST["map6"], "type" => $_POST["map6type"]));
+array_push($maps, array("url" => $_POST["map7"], "type" => $_POST["map7type"]));
+array_push($maps, array("url" => $_POST["map8"], "type" => $_POST["map8type"]));
+array_push($maps, array("url" => $_POST["map9"], "type" => $_POST["map9type"]));
+array_push($maps, array("url" => $_POST["map10"], "type" => $_POST["map10type"]));
+array_push($maps, array("url" => $_POST["map11"], "type" => $_POST["map11type"]));
 
 $db->query("INSERT INTO series (date, bestof, host, event, teamA, teamB, comment) VALUES ('" . $date . "', '" . $bestof . "', '" . $host . "', '" . $event . "', '" . $teamA . "', '" . $teamB . "', '" . $comment . "')");
 
@@ -34,7 +35,10 @@ $series = $row['id'];
 
 for ($i = 0; $i <= 10; $i++) {
     $j = $i + 1;
-    if ($maps[$i] != null || $maps[$i] != "") $db->query("INSERT INTO maps (number, url, series) VALUES ('" . $j . "', '" . $maps[$i] . "', '" . $series . "')");
+    if ($maps[$i]['url'] != null || $maps[$i]['url'] != "")
+    {
+        $db->query("INSERT INTO maps (number, url, series, type) VALUES ('" . $j . "', '" . $maps[$i]['url'] . "', '" . $series . "', '" . $maps[$i]['type'] . "')");
+    }
 }
 
 $content = $teamA . " vs. " . $teamB . " - " . $date;
